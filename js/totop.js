9f42436e3e3164bd7016edcbef470df3d778fe30
XiaoboSun <sunxb05@gmail.com>
2018-06-14 18:47:25 +0200

    Site updated: 2018-06-14 18:47:25

--- js/totop.js
+++
@@ -1,1 +0,0 @@
-(function(o){var e=1e3;var t=o("#totop");var l=500;t.hide();o(window).scroll(function(){var l=o(document).scrollTop();if(l>e){o(t).stop().fadeTo(300,1)}else{o(t).stop().fadeTo(300,0)}});o(t).click(function(){o("html, body").animate({scrollTop:0},l);return false})})(jQuery);
